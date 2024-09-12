de60a5a1075be64ee55c11f9fd5fa2412cc22a21
abhikgems <ak@gems1234>
2024-09-12 12:51:02 +0530

    Initial commit of the counter project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+    // setCounter(prevCounter => prevCounter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  useEffect(() => {
+    document.title = `Counter Demo`;
+  }, [counter]); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Gems Counter</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+    </>
   );
 }
 
